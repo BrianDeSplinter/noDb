77960d15d4805243ac20f689e3ebb75232f01b87
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-23 13:38:07 -0600

    Setup App.js & fixed favicon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios'
+import Header from './components/Header'
+import Lapboard from './components/Lapboard'
+import AddRacer from './components/AddRacer'
+import Racers from './components/Racers'
+import EditDelete from './components/EditDelete'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      racersAndLapTimes: []
+    }
+    //bind all functions here
+  }
+  
+  componentDidMount(){
+    axios.get('/api/scoreboard').then((res) => {
+      this.setState({
+        racersAndLapTimes: res.data
+      })
+    })
+  }
+
+  addNewRacer(racer){
+    const body = {racer}
+    axios.post('/api/scoreboard', body).then((res) => {
+      this.setState({
+        racersAndLapTimes: res.data
+      })
+    })
+  }
+
+  editLapTimes(id, updatedRacer){
+    const body = {updatedRacer}
+    axios.put(`/api/scoreboard/${id}`, body).then((res) => {
+      this.setState({
+        racersAndLapTimes: res.data
+      })
+    })
+  }
+
+  clearLapTimes(id, updatedRacer){
+    const body = {updatedRacer}
+    axios.put(`/api/scoreboard/${id}`, body).then((res) => {
+      this.setState({
+        racersAndLapTimes: res.data
+      })
+    })
+  }
+
+  addLap(){}
+
+  clearBoard(){}
+
+  deleteRacer(id){
+    axios.delete(`/api/scoreboard/${id}`).then((res) => {
+      this.setState({
+        racersAndLapTimes: res.data
+      })
+    })
+  }
+  
+  
+  
+  
+  render() {
+    return(
+      <div className="App">
+      <Header/>
+      </div>
+    )
+  };
 }
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header(){
+    return(
+        <header>
+            <h1>Lap Time Scoreboard</h1>
+        </header>
+    )
+}
+
+export default Header
