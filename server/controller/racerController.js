712a8a356d66430281eedc0ec28f662129eeee1e
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-23 18:51:59 -0600

    Backend fully functional

--- server/controller/racerController.js
+++ server/controller/racerController.js
@@ -7,26 +7,88 @@ module.exports = {
     },
 
     addRacer: (req,res) => {
-        
+        const laps = data[0].times.length
+        const newRacer = {
+            id, 
+            name: 'NewRacer',
+            times: []
+        }
+
+        for(let i = 0; i < laps; i++){
+            newRacer.times.push('_:_:_')
+        }
+
+        data.push(newRacer)
+
+        id++
+
+        res.status(201).send(data)
     },
 
-    // editLapTimes: (req, res) => {
+    editLapTimes: (req, res) => {
+        const {racer_id} = req.params
+        const {newName, times} = req.body
 
-    // },
+        const index = data.findIndex((e) => e.id == racer_id)
 
-    // clearLapTimes: (req, res) => {
+        if(index === -1){
+            res.status(404).send('Racer not found :(')
+        }
 
-    // },
+        data[index].name = newName
+        data[index].times = times
 
-    // addLap: (req, res) => {
+        res.status(202).send(data)
+    },
 
-    // },
+    clearLapTimes: (req, res) => {
+        const {racer_id} = req.params
 
-    // clearBoard: (req, res) => {
+        const index = data.findIndex((e) => e.id == racer_id)
 
-    // },
+        if(index === -1){
+            res.status(404).send('Racer not found :(')
+        }
 
-    // deleteRacer: (req, res) => {
+        const update = data[index].times.map((e) => '_:_:_')
 
-    // }
+        data[index].times= update
+
+        res.status(202).send(data)
+    },
+
+    addLap: (req, res) => {
+        const length = data.length
+        for(let i = 0; i < length; i++){
+            data[i].times.push('_:_:_')
+        }
+
+        res.status(201).send(data)
+    },
+
+    clearBoard: (req, res) => {
+        const length = data.length
+        
+        for(let i = 0; i < length; i++){
+            const update = data[i].times.map((e) => '_:_:_')
+            data[i].times = update
+            data[i].name = `Racer ${i+1}`
+        }
+
+        res.status(200).send(data)
+    },
+
+    deleteRacer: (req, res) => {
+        const {racer_id} = req.params
+
+        const index = data.findIndex((e) => e.id == racer_id)
+
+        if(index === -1){
+            res.status(404).send('Racer not found :(')
+        }
+
+        data.splice(index, 1)
+
+        res.status(200).send(data)
+    }
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-//const data = require('./data.js')
 const ctrl = require('./controller/racerController')
 const SERVER_PORT =  4334
 
@@ -8,8 +7,11 @@ app.use(express.json())
 
 app.get('/api/scoreboard', ctrl.getRacers)
 app.post('/api/scoreboard', ctrl.addRacer)
-// app.put('/api/scoreboard', ctrl.editLapTimes) // add params to link?
-// app.delete('/api/scoreboard', ctrl.deleteRacer)  // add params to link?
+app.put('/api/scoreboard/edit/:racer_id', ctrl.editLapTimes) 
+app.put('/api/scoreboard/clear/:racer_id', ctrl.clearLapTimes) 
+app.put('/api/scoreboard/addlap', ctrl.addLap) 
+app.put('/api/scoreboard/clearboard', ctrl.clearBoard)
+app.delete('/api/scoreboard/:racer_id', ctrl.deleteRacer) 
 
 app.listen(SERVER_PORT, () =>
     console.log(`Start your engines on port: ${SERVER_PORT}`)
