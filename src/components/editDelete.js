e7ea39425ab75de2a5fcdc9be099e5f26ae32bb6
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-24 21:03:02 -0600

    formatting

--- src/components/editDelete.js
+++ src/components/editDelete.js
@@ -28,16 +28,17 @@ class EditDelete extends Component{
     render(){
         return(
             <div key={this.props.racer.id}>
-                <button onClick={() => {if(window.confirm(`Are you sure you wish to delete ${this.props.racer.name}?`)){this.props.deleteRacer(this.props.racer.id)};}}>Delete Racer</button>
-
-
-                <button onClick={() => {if(window.confirm(`Are you sure you wish to clear ${this.props.racer.name}'s lap times?`)){this.props.clearLapTimes(this.props.racer.id)};}}>Clear Lap Times</button>
-                
-                
-                
+                <button 
+                    onClick={() => {if(window.confirm(`Are you sure you wish to delete ${this.props.racer.name}?`)){this.props.deleteRacer(this.props.racer.id)};}}
+                    >Delete Racer
+                </button>
+                <button 
+                    onClick={() => {if(window.confirm(`Are you sure you wish to clear ${this.props.racer.name}'s lap times?`)){this.props.clearLapTimes(this.props.racer.id)};}}
+                    >Clear Lap Times
+                </button>
                 <h1>{this.props.racer.name}</h1>
                 <h2>{this.props.racer.id}</h2>
-                <h3 style={{height: '40px', width: '65px', display: 'flex', flexDrection: 'row', justifyContent: 'space-between', backgroundColor: 'green'}}>{this.props.racer.times.map(e => {return <div>{e}</div>})}</h3>
+                <h3 style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>{this.props.racer.times.map(e => {return <div style={{height: '40px', width: '65px', backgroundColor: 'green'}}>{e}</div>})}</h3>
             </div>
         )
     }
