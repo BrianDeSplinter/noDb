d227cb5d2774804de62df1dc169434e9b40651a0
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-24 20:50:17 -0600

    formatting

--- src/components/addRacer.js
+++ src/components/addRacer.js
@@ -11,7 +11,8 @@ function AddRacer(props){
         <div>
             <button onClick= {() => props.addLap()}>Add New Lap</button>
             <button 
-                onClick= {() => {if(window.confirm(`Are you sure you wish to clear the scoreboard`)){props.clearBoard()};}}>Clear Board
+                onClick= {() => {if(window.confirm(`Are you sure you wish to clear the scoreboard`)){props.clearBoard()};}}
+                >Clear Board
             </button>
             <button onClick= {() => props.addNewRacer()}>Add New Racer</button>
         </div>
