f18905478fbf0df32e56d974b5009b6d031bf4bc
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-25 03:26:51 -0600

    Showing Multiple input fields, wont work...

--- src/components/RacerTimes.js
+++ src/components/RacerTimes.js
@@ -1,17 +1,64 @@
 import React, {Component} from 'react'
 
+//editLapTimes = {props.clearLapTimes}
+
 class RacerTimes extends Component{
     constructor(){
         super()
-        this.state = {
-
+        this.state= {
+            isEditing: false,
+            times: ''
         }
     }
 
+    toggleEdit(){
+        this.setState({
+            isEditing: !this.state.isEditing
+        })
+    }
 
+    handleChange(e){
+        this.setState({
+            userInput: e.target.value 
+        })
+    }
+
+    handleSaveTimes() {
+        const {racer} = this.props
+        const edited = {
+            "newName": null,
+            "times": this.state.userInput
+        }
+        this.props.editLapTimes(racer.id, edited)
+        this.toggleEdit()
+    }
     render(){
         return(
-          <div style={{height: '40px', width: '65px', backgroundColor: 'green', display: 'flex', flexDirection: 'row', justifyContent:'space-between'}}>{this.props.racer.times.map(e => {return <div style={{height: '40px', width: '65px', backgroundColor: 'green'}}>{e}</div>})}</div>
+          <div> 
+           
+           
+            <div>
+                {!this.state.isEditing ? (
+                <div style={{display: 'flex', flexDirection: 'row', justifyContent:'space-between'}}>
+                {this.props.racer.times.map(e => {return <div style={{height: '40px', width: '65px', backgroundColor: 'green'}}>
+                    {e}</div>})}
+                    <button onClick={() => this.toggleEdit()}>Edit Lap Times</button>
+                </div>
+            ) : (
+                <div style={{display:'flex', flexDirection:'column'}}>{this.props.racer.times.map((e,i) => <input onChange={(event) => this.handleChange(event)} value={this.props.racer.times[i]}/>)}
+                    
+                    <button onClick={() => this.handleSaveTimes()}>Confirm Time Changes</button>
+                    <button onClick={() => this.toggleEdit()}>Cancel Time Changes</button>
+                </div>
+            )}
+            </div>
+        
+        
+        
+                
+        
+        
+        </div>
         )
     }
 }
