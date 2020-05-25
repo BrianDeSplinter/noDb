622e7d3b12c26f14e2e0a54851f9a7dc33921a55
BrianDeSplinter <bdesplinter34@gmail.com>
2020-05-24 20:48:13 -0600

    Added confirm to clearboard

--- src/components/addRacer.js
+++ src/components/addRacer.js
@@ -7,11 +7,12 @@ import React from 'react'
 function AddRacer(props){
 
 
-
     return(
         <div>
             <button onClick= {() => props.addLap()}>Add New Lap</button>
-            <button onClick= {() => props.clearBoard()}>Clear Board</button>
+            <button 
+                onClick= {() => {if(window.confirm(`Are you sure you wish to clear the scoreboard`)){props.clearBoard()};}}>Clear Board
+            </button>
             <button onClick= {() => props.addNewRacer()}>Add New Racer</button>
         </div>
     )
